f6621beb99dbeb4ab2ed429ec793fe25528c660d
fullangel <99usui99@gmail.com>
2021-03-31 11:28:25 -0400

    changes in front

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,11 @@
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import { productListReducer } from './constants/productReducers';
+import { productListReducer, productDetailsReducer } from './reducers/productReducers';
 
 const initialState = {};
 const reducer = combineReducers({
     productList: productListReducer,
+    productDetails: productDetailsReducer,
 });
 
 const composeEnhancer = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
