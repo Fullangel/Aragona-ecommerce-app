7779453a41f2b41e7488933151ff198e419b9509
fullangel <99usui99@gmail.com>
2021-03-31 22:32:44 -0400

    changes in front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
@@ -20,8 +21,9 @@ function App() {
         </div>
       </header>
       <main>
-        <Route path="/product/:id" component={ ProductScreen }></Route>
-        <Route path="/" component={ HomeScreen } exact></Route>
+        <Route path="/cart/:id?" component={ CartScreen } />
+        <Route path="/product/:id" component={ ProductScreen } />
+        <Route path="/" component={ HomeScreen } exact />
 
       </main>
       <footer className="row center">
